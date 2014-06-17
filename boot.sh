f5916fd5282468133123c089453c3380599729bc
João Paiva <jgpaiva@gsd.inesc-id.pt>
2014-06-17 19:55:17 +0100

    correct boot of nodes

--- boot.sh
+++ boot.sh
@@ -1,4 +1,10 @@
-for i in 01 02 03 21 22 23 24 25
+for i in 01 02 03 04 05 06 07 08 21 22 23 24 25 26 27 28
+do 
+    nova boot --image "jgpaiva-clean" --flavor="10" jgpaiva${i}_1 --availability-zone nova:node$i
+    sleep 2
+done
+
+for i in 30 31 32 33
 do 
     nova boot --image "jgpaiva-clean" --flavor="14" jgpaiva${i}_1 --availability-zone nova:node$i
     sleep 2
