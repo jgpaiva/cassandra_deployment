01bf52e3467b79843ff31e278251b347a657bd70
João Paiva <jgpaiva@gsd.inesc-id.pt>
2014-06-24 19:54:30 +0100

    simpler benchmark

--- benchmark.py
+++ benchmark.py
@@ -1,108 +1,41 @@
 def set_pars():
     from environment import cassandra_settings
     cassandra_settings.run_original = False
-    cassandra_settings.threads = 500
-    #cassandra_settings.operationcount = 5000000
     #cassandra_settings.recordcount = 1000000
-    cassandra_settings.operationcount = 50000
+    #cassandra_settings.operationcount = 200000
     cassandra_settings.recordcount = 1
     cassandra_settings.timeout = 60*40 # 40mins
-    cassandra_settings.data_placement_rounds_duration = 2000 #2 sec
     cassandra_settings.save_ops = True
     cassandra_settings.debug_logs = False
-    cassandra_settings.write_consistency = 'ONE'
-    cassandra_settings.readproportion = '0.99'
-    cassandra_settings.updateproportion = '0.01'
-
+    cassandra_settings.write_consistency = 'ALL'
+    cassandra_settings.readproportion = '0.95'
+    cassandra_settings.updateproportion = '0.05'
+    cassandra_settings.sleep_time = 0
+    cassandra_settings.ycsb_nodes = 4
 
 def configs():
     from environment import cassandra_settings as settings
-    for i in range(3):
 
-        settings.replication_factor = 1
-        settings.large_replication_degree = 2
-        settings.max_items_for_large_replication_degree = 40
-        yield i==0 #do prepare first
+    yield False # do prepare first?
 
-        settings.replication_factor = 1
-        settings.large_replication_degree = 1
-        settings.max_items_for_large_replication_degree = 0
-        yield False #avoid prepare first
+    #for sleep_time, divisor in [(0,1000000),(1,100000),(10,1000)]:
+    for sleep_time, operations in [(3,150000)]:
+        for threads in [2000, 8000, 16000]:
+            settings.operationcount = operations
+            settings.threads = threads
+            settings.sleep_time = sleep_time
 
-        settings.replication_factor = 2
-        settings.large_replication_degree = 2
-        settings.max_items_for_large_replication_degree = 0
-        yield False #avoid prepare first
+            settings.replication_factor = 1
+            settings.large_replication_degree = 1
+            settings.max_items_for_large_replication_degree = 0
+            yield False
 
-        settings.replication_factor = 2
-        settings.large_replication_degree = 4
-        settings.max_items_for_large_replication_degree = 40
-        yield False #avoid prepare first
+            settings.replication_factor = 2
+            settings.large_replication_degree = 2
+            settings.max_items_for_large_replication_degree = 0
+            yield False
 
-        settings.replication_factor = 2
-        settings.large_replication_degree = 4
-        settings.max_items_for_large_replication_degree = 5
-        yield False #avoid prepare first
-
-        settings.replication_factor = 3
-        settings.large_replication_degree = 3
-        settings.max_items_for_large_replication_degree = 0
-        yield False #avoid prepare first
-
-        settings.replication_factor = 3
-        settings.large_replication_degree = 6
-        settings.max_items_for_large_replication_degree = 40
-        yield False #avoid prepare first
-
-        settings.replication_factor = 4
-        settings.large_replication_degree = 4
-        settings.max_items_for_large_replication_degree = 0
-        yield False #avoid prepare first
-
-        settings.replication_factor = 1
-        settings.large_replication_degree = 4
-        settings.max_items_for_large_replication_degree = 40
-        yield False #avoid prepare first
-
-        settings.replication_factor = 2
-        settings.large_replication_degree = 8
-        settings.max_items_for_large_replication_degree = 40
-        yield False #avoid prepare first
-
-        settings.replication_factor = 2
-        settings.large_replication_degree = 16
-        settings.max_items_for_large_replication_degree = 40
-        yield False #avoid prepare first
-
-        settings.replication_factor = 2
-        settings.large_replication_degree = 8
-        settings.max_items_for_large_replication_degree = 5
-        yield False #avoid prepare first
-
-        settings.replication_factor = 2
-        settings.large_replication_degree = 16
-        settings.max_items_for_large_replication_degree = 5
-        yield False #avoid prepare first
-
-        settings.replication_factor = 8
-        settings.large_replication_degree = 8
-        settings.max_items_for_large_replication_degree = 0
-        yield False #avoid prepare first
-
-#    settings.run_original = True
-#    f.prepare()
-#    for i in range(3):
-#        settings.replication_factor = 1
-#        f.benchmark_round()
-#
-#        settings.replication_factor = 2
-#        f.benchmark_round()
-#
-#        settings.replication_factor = 3
-#        f.benchmark_round()
-#
-#        settings.replication_factor = 4
-#        f.benchmark_round()
-#
-#        settings.replication_factor = 8
-#        f.benchmark_round()
+            settings.replication_factor = 8
+            settings.large_replication_degree = 8
+            settings.max_items_for_large_replication_degree = 0
+            yield False
