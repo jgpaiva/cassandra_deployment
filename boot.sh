323691128634d11401bb78b7462b21d4dc3d314c
João Paiva <jgpaiva@gsd.inesc-id.pt>
2014-07-02 14:13:41 +0100

    boot one machine per node

--- boot.sh
+++ boot.sh
@@ -1,17 +1,11 @@
-for i in 01 02 03 04 05 06 07 08 21 22 23 24 25 26 27 28
+for i in 01 02 03 04 05 06 07 08 21 22
 do 
     nova boot --image "jgpaiva-clean" --flavor="10" jgpaiva${i}_1 --availability-zone nova:node$i
     sleep 2
 done
 
-for i in 30 31 32 33
+for i in 31 32 33 35
 do 
-    nova boot --image "jgpaiva-clean" --flavor="14" jgpaiva${i}_1 --availability-zone nova:node$i
-    sleep 2
-    nova boot --image "jgpaiva-clean" --flavor="14" jgpaiva${i}_2 --availability-zone nova:node$i
-    sleep 2
-    nova boot --image "jgpaiva-clean" --flavor="14" jgpaiva${i}_3 --availability-zone nova:node$i
-    sleep 2
-    nova boot --image "jgpaiva-clean" --flavor="14" jgpaiva${i}_4 --availability-zone nova:node$i
+    nova boot --image "jgpaiva-clean" --flavor="10" jgpaiva${i}_1 --availability-zone nova:node$i
     sleep 2
 done
